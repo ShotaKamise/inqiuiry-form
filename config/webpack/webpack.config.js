2f417e7a965392a46b3e01fd0326d2ff0bd0bffd
ShotaKamise <s.k.k.s0316@gmail.com>
2024-02-25 14:21:09 +0900

    webpackの設定にbabel-loaderを追加

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,34 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
+// const { generateWebpackConfig } = require('shakapacker')
 
-const webpackConfig = generateWebpackConfig()
+// const webpackConfig = generateWebpackConfig()
 
-module.exports = webpackConfig
+// module.exports = webpackConfig
+
+const { generateWebpackConfig } = require('shakapacker');
+const { merge } = require('webpack-merge');
+
+const customConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
+
+const webpackConfig = generateWebpackConfig();
+const finalConfig = merge(webpackConfig, customConfig);
+
+module.exports = finalConfig;
+
